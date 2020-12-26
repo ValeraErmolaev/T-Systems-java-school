a8b10c036ce750cb049cf61ca1efc4f285bb6c81
ValeraErmolaev <ermolaev.1995@gmail.com>
2020-12-26 03:10:55 +0300

    Spring config without xml. Home page, a little bit js.

--- src/main/java/vermolae/config/WebConfig.java
+++ src/main/java/vermolae/config/WebConfig.java
@@ -3,7 +3,9 @@ package vermolae.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -17,5 +19,9 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         resolver.setPrefix("/");
         resolver.setSuffix(".jsp");
         return resolver;
+    }
+    // Static Resource Config
+    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
 }
