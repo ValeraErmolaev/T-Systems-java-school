898ca9b5bcc8a108c0fa952e22c8c0f07155bc23
ValeraErmolaev <ermolaev.1995@gmail.com>
2021-01-08 01:04:08 +0300

    update dto

--- src/main/java/vermolae/model/dto/User/UserRegistrationForm.java
+++ src/main/java/vermolae/model/dto/User/UserRegistrationForm.java
@@ -25,4 +25,57 @@ public class UserRegistrationForm implements Serializable {
 
     @NotBlank(message = "Confirm password")
     private String confirmPassword;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getConfirmEmail() {
+        return confirmEmail;
+    }
+
+    public void setConfirmEmail(String confirmEmail) {
+        this.confirmEmail = confirmEmail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public UserRegistrationForm() {
+    }
+
+    public UserRegistrationForm(@NotBlank(message = "Set First name") @Pattern(regexp = "[a-zA-Z]", message = "Latin letters are allowed") String firstname, @NotBlank(message = "Set email") @Pattern(regexp = "^([A-Za-z0-9+_.-]+@(.+))?$",
+            message = "Latin letters, digits, dashes, underscores and points are allowed") String email, @NotBlank(message = "Confirm email") @Pattern(regexp = "^([A-Za-z0-9+_.-]+@(.+))?$",
+            message = "Latin letters, digits, dashes, underscores and points are allowed") String confirmEmail, @NotBlank(message = "Set password") String password, @NotBlank(message = "Confirm password") String confirmPassword) {
+        this.firstname = firstname;
+        this.email = email;
+        this.confirmEmail = confirmEmail;
+        this.password = password;
+        this.confirmPassword = confirmPassword;
+    }
 }
